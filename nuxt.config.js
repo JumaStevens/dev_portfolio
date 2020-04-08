b780daf71c4ec25a8e4848bfae57a93abdd60bed
Juma Stevens <jumastevens@gmail.com>
2020-04-08 11:25:08 -0400

    update project configs

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,70 +1,73 @@
 const path = require('path')
-const pkg = require('./package')
-
 const isProd = process.env.NODE_ENV === 'production'
 
 
 module.exports = {
   mode: 'universal',
-
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
-    title: pkg.name,
+    title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
+      }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
-
   /*
-  ** Customize the progress-bar color
-  */
+   ** Customize the progress-bar color
+   */
   loading: { color: '#fff' },
-
   /*
-  ** Global CSS
-  */
+   ** Global CSS
+   */
   css: [
     '@/assets/sass/main.sass',
-
-    { src: '~/node_modules/highlight.js/styles/github.css', lang: 'css' }
+    {
+      src: '~/node_modules/highlight.js/styles/github.css',
+      lang: 'css'
+    }
   ],
-
   /*
-  ** Plugins to load before mounting the App
-  */
+   ** Plugins to load before mounting the App
+   */
   plugins: [
     { src: '~/plugins/vue-lazyload', ssr: false },
     { src: '~/plugins/vue-scrollto.js', ssr: false },
   ],
-
   /*
-  ** Nuxt.js modules
-  */
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/eslint-module
+    // '@nuxtjs/eslint-module',
+    // Doc: https://github.com/nuxt-community/stylelint-module
+    // '@nuxtjs/stylelint-module'
+  ],
+  /*
+   ** Nuxt.js modules
+   */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
-
     '@nuxtjs/pwa',
-
     '@nuxtjs/style-resources',
-
     '@nuxtjs/markdownit',
-
+    '@nuxtjs/moment',
     'nuxt-validate',
-
+    'nuxt-svg-loader',
     'nuxt-client-init-module',
-
     'nuxt-vuex-router-sync',
   ],
-
   /*
-  ** Markdown options
-  */
+   ** Markdown options
+   */
   markdownit: {
     injected: false,
     linkify: true,
@@ -72,56 +75,33 @@ module.exports = {
       'markdown-it-highlightjs'
     ]
   },
-
-
   /*
-  ** Style resource settings
-  */
+   ** Style resource settings
+   */
   styleResources: {
-    sass: [
-      '~/assets/sass/global.sass'
-    ]
+    sass: ['@/assets/sass/global.sass']
   },
-
   /*
-  ** Axios module configuration
-  */
-  axios: {
-    // See https://github.com/nuxt-community/axios-module#options
-  },
-
+   ** Axios module configuration
+   ** See https://axios.nuxtjs.org/options
+   */
+  axios: {},
   /*
-  ** Build configuration
-  */
+   ** Define the development or production mode of Nuxt.js
+   ** See https://nuxtjs.org/api/configuration-dev
+   */
+  dev: !isProd,
+  /*
+   ** Build configuration
+   */
   build: {
+    extractCSS: isProd,
     /*
-    ** You can extend webpack config here
-    */
+     ** You can extend webpack config here
+     */
     extend(config, ctx) {
-      // // Run ESLint on save
-      // if (ctx.isDev && ctx.isClient) {
-      //   config.module.rules.push({
-      //     enforce: 'pre',
-      //     test: /\.(js|vue)$/,
-      //     loader: 'eslint-loader',
-      //     exclude: /(node_modules)/,
-      //     options: {
-      //       fix: true
-      //     }
-      //   })
-      // }
-
-
       // Custom alias
       config.resolve.alias['~comp'] = path.resolve(__dirname, 'nuxt', 'components')
-
-      // Load SVG as Vue Components
-      const svgRule = config.module.rules.find(rule => rule.test.test('.svg'))
-      svgRule.test = /\.(png|jpe?g|gif|webp)$/
-      config.module.rules.push({
-        test: /\.svg$/,
-        loader: 'vue-svg-loader',
-      })
     }
   }
 }
